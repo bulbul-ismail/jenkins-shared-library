eda9e866e5e3d4cdb6dd3501c066028bbfee572c
ismail <bulbull.ismail@gmail.com>
2020-07-02 13:51:59 +0300

    Create basePipeline.groovy

---
+++ vars/basePipeline.groovy
@@ -0,0 +1,58 @@
+def call(body) {
+    def pipelineParams= [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = pipelineParams
+    body()
+
+def label = "worker-${UUID.randomUUID().toString()}"
+def serviceAccount = "${env.SERVICE_ACCOUNT}"
+def mavenImage = "${env.MAVEN_IMAGE}"
+
+podTemplate(
+    label: label, 
+    serviceAccount: serviceAccount,
+    containers: [
+        containerTemplate(name: 'maven', image: mavenImage, command: 'cat', ttyEnabled: true),
+        containerTemplate(name: 'docker', image: 'docker' command: 'cat', ttyEnabled: true),
+        containerTemplate(name: 'helm', image: 'lachlanevenson/k8s-helm:latest', command: 'cat', ttyEnabled: true)
+    ],
+    imagePullSecrets: ["${env.REGISTRY_SECRET}"],
+    volumes: [
+        hostPathVolume(mountPath: '/var/run/docker.sock', hostPath: '/var/run/docker.sock')
+    ]
+) {
+    node(label) {
+        stage('Checkout'){
+            checkout scm
+        }
+        stage('Mvn Build'){
+            container('maven'){
+                sh "mvn clean install -DskipTests=true"
+            }
+        }
+        stage('Docker build') {
+            container('docker') {
+               customImage = docker.build("${env.IMAGE_REGISTRY}/${env.IMAGE_REGISTRY_REPOSITORY_NAME}/${env.PROJECT_NAME}:${env.BUILD_ID}")
+            }
+        }
+        stage('Docker push') {
+            container('docker') {
+                docker.withRegistry("${env.IMAGE_REGISTRY_URL}", "${env.IMAGE_REGISTRY_CREDENTIAL_ID}") {
+                    customImage.push()
+                }
+            }
+        }
+        stage('Checkout Config Repo') {
+            sh "mkdir conf"
+            dir("conf"){
+                checkout([$class: 'GitSCM', branches: [[name: "*/${env.CONFIG_REPO_BRANCH}"]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'SparseCheckoutPaths', sparseCheckoutPaths: [[path: 'config/']]]], submoduleCfg: [], userRemoteConfigs: [[credentialsId: "${env.SCM_CREDENTIAL_ID}", url: "${env.CONFIG_REPO_URL}"]]])
+            }
+        }
+        stage('Deploy') {
+            container('helm') {
+                sh "helm install --set-file properties.file.base=./conf/config/${env.PROJECT_NAME}/${env.PROJECT_NAME}.yml,properties.file.env=./conf/config/${env.PROJECT_NAME}/${env.PROJECT_NAME}-${env.ENVIRONMENT}.yml ${env.PROJECT_NAME} ./${env.HELM_CHART_NAME}"
+            }
+        }
+    }
+}
+}
